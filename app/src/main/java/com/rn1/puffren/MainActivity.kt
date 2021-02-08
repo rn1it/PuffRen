311eea40e58f5a313a20140d5bfc2828c9025d80
RN1 <gary13900103@gmail.com>
2021-02-08 16:48:27 +0800

    update version to 23, add menu item click effect, change status bar

--- app/src/main/java/com/rn1/puffren/MainActivity.kt
+++ app/src/main/java/com/rn1/puffren/MainActivity.kt
@@ -2,11 +2,24 @@ package com.rn1.puffren
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setupStatusBar()
     }
+
+    private fun setupStatusBar(){
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = resources.getColor(R.color.orange_ffa626)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
 }
 
