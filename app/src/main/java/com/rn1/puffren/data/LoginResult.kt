4eef264900355917c26b626c242134a2db52cb6f
RN1 <gary13900103@gmail.com>
2021-05-23 00:22:27 +0800

    loginResult add userId

--- app/src/main/java/com/rn1/puffren/data/LoginResult.kt
+++ app/src/main/java/com/rn1/puffren/data/LoginResult.kt
@@ -2,5 +2,6 @@ package com.rn1.puffren.data
 
 data class LoginResult(
     val error: String? = null,
-    val accessToken: String? = null
+    val accessToken: String? = null,
+    val userId: String? = null
 )
