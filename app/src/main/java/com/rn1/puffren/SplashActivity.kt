13cb3811c1b3f5f4bf6530e97c967716507d3cca
RN1 <gary13900103@gmail.com>
2021-06-22 22:54:18 +0800

    add splash activity

--- app/src/main/java/com/rn1/puffren/SplashActivity.kt
+++ app/src/main/java/com/rn1/puffren/SplashActivity.kt
@@ -3,11 +3,15 @@ package com.rn1.puffren
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },1500)
     }
 }
